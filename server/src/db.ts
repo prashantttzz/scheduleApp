285d5213f2a86e57390b405e9b2522091f197ffd
prashantttzz <contact.prashant31@gmail.com>
2025-09-22 00:07:13 +0530

    type error fix

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,7 @@
 import knex from 'knex';
 import config from './knexfile.js';
 
-const environment = process.env.NODE_ENV || 'development';
-const db = knex(config[environment]);
+const env = (process.env.NODE_ENV || "development") as "development" | "production";
+const db = knex(config[env]);
 
 export default db;
