65633a61cdd12ac1ca8bf40f9a3ddc3dbb86c6fa
prashantttzz <contact.prashant31@gmail.com>
2025-09-22 00:11:47 +0530

    cors error fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://schedule-app-jade.vercel.app",
     credentials: true,
   })
 );
